320625e438f7bb6c4b455ac957dbdc247739a411
IvanR96 <irios96@hotmail.com>
2025-10-10 16:25:15 -0700

    node js code

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const fs = require("fs");
 
-/*fs.writeFile("message.txt", "Hello from NodeJS!!", (err) =>{
-    if(err) throw err;
-    console.log("The file has been saved!");
+fs.writeFile("message.txt", "Hello Node.js Welcome", 'utf-8', (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
 });
-*/
-
-fs.readFile('message.txt',"utf-8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }); 
